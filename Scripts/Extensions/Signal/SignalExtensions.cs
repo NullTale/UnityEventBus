118969e5e1a3b137da47cf91beb55a0b41c750c5
Null Tale <1497430+NullTale@users.noreply.github.com>
2022-03-04 11:00:52 +0300

    refactor: signal extentions naming

--- Scripts/Extensions/Signal/SignalExtensions.cs
+++ Scripts/Extensions/Signal/SignalExtensions.cs
@@ -1,12 +1,38 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine.Timeline;
+using Object = UnityEngine.Object;
 
 namespace UnityEventBus
 {
     public static class SignalExtensions
     {
-        public static void Rise(this SignalAsset signal)
+        private static Dictionary<(SignalAsset, Action), GenericListener<SignalAsset>> s_SignalListeners    = new Dictionary<(SignalAsset, Action), GenericListener<SignalAsset>>(32);
+        private const  string                                                          k_SignalListenerName = "GSL";
+
+        // =======================================================================
+        public static void Invoke(this SignalAsset signal)
         {
             GlobalBus.Send(in signal);
+        }
+        
+        // questionable
+        private static void Subscribe(this SignalAsset signal, Action action, int order)
+        {
+            var signalListener = new GenericListener<SignalAsset>(s =>
+            {
+                if (s == signal)
+                    action.Invoke();
+            }, order, k_SignalListenerName);
+
+            GlobalBus.Subscribe(signalListener);
+            s_SignalListeners.Add((signal, action), signalListener);
+        }
+
+        private static void UnSubscribe(this SignalAsset signal, Action action)
+        {
+            if (s_SignalListeners.Remove((signal, action), out var signalListener))
+                GlobalBus.UnSubscribe(signalListener);
         }
     }
 }
